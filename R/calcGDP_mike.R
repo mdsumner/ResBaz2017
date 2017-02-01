35cba00e23d523dff02cfca061758b34e9d5dc10
Michael Sumner <mdsumner@gmail.com>
2017-02-01 16:44:11 +1100

    first functioning package

--- R/calcGDP_mike.R
+++ R/calcGDP_mike.R
@@ -1,5 +1,14 @@
-# Takes a dataset and multiplies the population column
-# with the GDP per capita column.
+#' Calculate GDP
+#' 
+#' Calculate the Gross Domestic Product
+#' 
+#' Takes population and gdp percapita and multiplies them together
+#' 
+#' @param population the population value
+#' @param gdpPercapita the gdp value
+#' @export 
+#' @examples 
+#' calcGDP_mike(10, 2.3)
 calcGDP_mike <- function(population, gdpPercapita) {
 
   ## do the calculation
